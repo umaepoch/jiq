63ad7a288386d72fa230512808238033a92a7d04
umaepoch <umaepoch@consulting.in>
2021-02-17 11:56:30 +0530

    validation code part removed

--- jiq/api.py
+++ jiq/api.py
@@ -97,34 +97,6 @@ def userlist(user):
     #print ("names----",names)
     return names
 
-@frappe.whitelist()
-@frappe.validate_and_sanitize_search_inputs
-def user_query(doctype, txt, searchfield, start, page_len, filters):
-	from frappe.desk.reportview import get_match_cond
 
-	user_type_condition = "and user_type = 'Website User'"
-	if filters and filters.get('ignore_user_type'):
-		user_type_condition = ''
-
-	txt = "%{}%".format(txt)
-	return frappe.db.sql("""SELECT `name`, CONCAT_WS(' ', first_name, middle_name, last_name)
-		FROM `tabUser`
-		WHERE `enabled`=1
-			{user_type_condition}
-			AND `docstatus` < 2
-			AND `name` NOT IN ({standard_users})
-			AND ({key} LIKE %(txt)s
-				OR CONCAT_WS(' ', first_name, middle_name, last_name) LIKE %(txt)s)
-			{mcond}
-		ORDER BY
-			CASE WHEN `name` LIKE %(txt)s THEN 0 ELSE 1 END,
-			CASE WHEN concat_ws(' ', first_name, middle_name, last_name) LIKE %(txt)s
-				THEN 0 ELSE 1 END,
-			NAME asc
-		LIMIT %(page_len)s OFFSET %(start)s""".format(
-			user_type_condition = user_type_condition,
-			standard_users=", ".join([frappe.db.escape(u) for u in STANDARD_USERS]),
-			key=searchfield, mcond=get_match_cond(doctype)),
-			dict(start=start, page_len=page_len, txt=txt))
     
 
